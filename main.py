1c1c3548fc9358d1b7f4b2fa8176768312a7f0b5
maduvitorino <maria.vitorino@gran.com>
2024-08-23 13:51:05 -0300

    docs: add prints just to recognize when function works

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Script iniciado no Cloud Run!"
+    print("Iniciando projeto no cloud run")
+    return "Script finalizado no Cloud Run!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
